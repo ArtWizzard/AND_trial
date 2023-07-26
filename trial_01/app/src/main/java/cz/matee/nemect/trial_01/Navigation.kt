2c3e620ade07c43c84003b5ff3dd35930c2ab493
Tomas Nemecek <nemect20@fit.cvut.cz>
2023-07-26 09:14:02 +0200

    main: Add navigation and configure MainActivity
    Navigation contains two basic screens.
    Main Activity runs navigation.

--- trial_01/app/src/main/java/cz/matee/nemect/trial_01/Navigation.kt
+++ trial_01/app/src/main/java/cz/matee/nemect/trial_01/Navigation.kt
@@ -1,2 +1,32 @@
 package cz.matee.nemect.trial_01
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import cz.matee.nemect.trial_01.presentation.chats.ChatsScreen
+import cz.matee.nemect.trial_01.presentation.conversation.ConversationScreen
+
+sealed class Screens( val route: String ) {
+    object ChatsScreen : Screens("chats")
+
+    object ConversationScreen : Screens("conversation")
+}
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screens.ChatsScreen.route ) {
+
+        composable(route = Screens.ChatsScreen.route) {
+            ChatsScreen(navController = navController)
+        }
+
+        composable(route = Screens.ConversationScreen.route) {
+            ConversationScreen(navController = navController)
+        }
+    }
+}
