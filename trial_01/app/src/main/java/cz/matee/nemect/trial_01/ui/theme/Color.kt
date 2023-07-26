08925bba7ea5856629846b59289b997e19ab1402
Tomas Nemecek <nemect20@fit.cvut.cz>
2023-07-26 09:12:56 +0200

    main: Configure basic colours

--- trial_01/app/src/main/java/cz/matee/nemect/trial_01/ui/theme/Color.kt
+++ trial_01/app/src/main/java/cz/matee/nemect/trial_01/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val White = Color(0xffffffff)
+
+val SelectedIcon = Color(0xff777777)
+val UnselectedIcon = Color(0xffbbbbbb)
+val BackgroundIcon = Color(0xffaaaaaa)
+
+val HeaderTextColor = Color(0xff000000)
+val HighlightedTextColor = Color(0xff000000)
+val NormalTextColor = Color(0xff222222)
+val ParagraphTextColor = Color(0xff444444)
+val NoteTextColor = Color(0xff999999)
