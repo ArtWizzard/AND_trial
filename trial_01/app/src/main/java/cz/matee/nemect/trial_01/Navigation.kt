3e2c42c1226bb7d4395875f1857e943f8fcc1b18
Tomas Nemecek <nemect20@fit.cvut.cz>
2023-07-28 18:28:13 +0200

    main: Add navigation
    Navigation can switch between chats and conversations.

--- trial_01/app/src/main/java/cz/matee/nemect/trial_01/Navigation.kt
+++ trial_01/app/src/main/java/cz/matee/nemect/trial_01/Navigation.kt
@@ -1,9 +1,11 @@
 package cz.matee.nemect.trial_01
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import cz.matee.nemect.trial_01.presentation.chats.ChatsScreen
 import cz.matee.nemect.trial_01.presentation.conversation.ConversationScreen
 
@@ -25,8 +27,16 @@ fun Navigation() {
             ChatsScreen(navController = navController)
         }
 
-        composable(route = Screens.ConversationScreen.route) {
-            ConversationScreen(navController = navController)
+        composable(
+            route = Screens.ConversationScreen.route + "/{id}" ,
+            arguments = listOf(
+                navArgument(name = "id") {
+                    type = NavType.IntType
+                }
+            )
+        ) {
+            entry ->
+            ConversationScreen(navController = navController, id = entry.arguments?.getInt("id"))
         }
     }
 }
