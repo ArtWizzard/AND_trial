70e214d07e1082e96b4658e6f22c429cbc664043
Tomas Nemecek <nemect20@fit.cvut.cz>
2023-07-28 18:29:23 +0200

    main: Configure and set main colors.
    Created basic color set with shades of black and white.

--- trial_01/app/src/main/java/cz/matee/nemect/trial_01/ui/theme/Color.kt
+++ trial_01/app/src/main/java/cz/matee/nemect/trial_01/ui/theme/Color.kt
@@ -2,19 +2,21 @@ package cz.matee.nemect.trial_01.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//val Purple80 = Color(0xFFD0BCFF)
+//val PurpleGrey80 = Color(0xFFCCC2DC)
+//val Pink80 = Color(0xFFEFB8C8)
+//
+//val Purple40 = Color(0xFF6650a4)
+//val PurpleGrey40 = Color(0xFF625b71)
+//val Pink40 = Color(0xFF7D5260)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val WhiteF = Color(0xffffffff)
+val WhiteB = Color(0xffbbbbbb)
+val WhiteA = Color(0xffaaaaaa)
+val White7 = Color(0xff777777)
+val White3 = Color(0xff333333)
+val White1 = Color(0xff111111)
 
-val White = Color(0xffffffff)
-
-val SelectedIcon = Color(0xff777777)
-val UnselectedIcon = Color(0xffbbbbbb)
-val BackgroundIcon = Color(0xffaaaaaa)
 
 val HeaderTextColor = Color(0xff000000)
 val HighlightedTextColor = Color(0xff000000)
