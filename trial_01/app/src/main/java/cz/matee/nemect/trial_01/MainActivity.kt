40100d0ab99c8f2f60ae5ef341b471accec739f7
Tomas Nemecek <nemect20@fit.cvut.cz>
2023-08-02 14:22:46 +0200

    main: Sets app to apply MaterialTheme

--- trial_01/app/src/main/java/cz/matee/nemect/trial_01/MainActivity.kt
+++ trial_01/app/src/main/java/cz/matee/nemect/trial_01/MainActivity.kt
@@ -3,12 +3,15 @@ package cz.matee.nemect.trial_01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import cz.matee.nemect.trial_01.ui.theme.Trial_01Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Navigation()
+            Trial_01Theme {
+                Navigation()
+            }
         }
     }
 }
