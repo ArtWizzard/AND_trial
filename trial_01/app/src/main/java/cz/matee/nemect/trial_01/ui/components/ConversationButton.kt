b4784600108caa6060c98854054519a8652b78f4
Tomas Nemecek <nemect20@fit.cvut.cz>
2023-08-02 14:23:38 +0200

    main: Create two frequent component prefabs
    Components:
    	- MessageBubble ... contains basic behaviour and appearance of
    	  text bubble in user's conversations
    	- ConversationButton ... basic appearance of buttons in user's
    	  conversation screen.

--- trial_01/app/src/main/java/cz/matee/nemect/trial_01/ui/components/ConversationButton.kt
+++ trial_01/app/src/main/java/cz/matee/nemect/trial_01/ui/components/ConversationButton.kt
@@ -1,15 +1,26 @@
 package cz.matee.nemect.trial_01.ui.components
 
-import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.core.graphics.drawable.IconCompat.IconType
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 
 @Composable
 fun ConversationButton (
-    icon: ,
+    icon: Int,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
     contentDescription: String? = null
 ) {
-    androidx.compose.material3.Icon(
-        imageVector = icon,
-        contentDescription = contentDescription)
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            painter = painterResource(id = icon),
+            contentDescription = contentDescription,
+            tint = MaterialTheme.colorScheme.primary)
+    }
 }
--- trial_01/app/src/main/java/cz/matee/nemect/trial_01/ui/components/MessageBubble.kt
+++ trial_01/app/src/main/java/cz/matee/nemect/trial_01/ui/components/MessageBubble.kt
@@ -1,2 +1,25 @@
 package cz.matee.nemect.trial_01.ui.components
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MessageBubble(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+        TextButton(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary),
+            shape = RoundedCornerShape(20.dp),
+            modifier = modifier
+        ) {
+            Text(
+                text = text,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+        }
+}
