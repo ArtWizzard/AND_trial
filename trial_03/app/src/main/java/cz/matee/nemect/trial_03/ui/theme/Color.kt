157fb6d39bdd171aa5819cb1a58e3f33cd7822bc
Tomas Nemecek <nemect20@fit.cvut.cz>
2023-08-03 11:17:38 +0200

    trial_03: Initial commit
    Basic configuration of colors and themes.
    Add implementation - coil, compose navigation

---
+++ trial_03/app/src/main/java/cz/matee/nemect/trial_03/ui/theme/Color.kt
@@ -0,0 +1,38 @@
+package cz.matee.nemect.trial_03.ui.theme
+
+import androidx.compose.ui.graphics.Color
+//
+//val Purple80 = Color(0xFFD0BCFF)
+//val PurpleGrey80 = Color(0xFFCCC2DC)
+//val Pink80 = Color(0xFFEFB8C8)
+//
+//val Purple40 = Color(0xFF6650a4)
+//val PurpleGrey40 = Color(0xFF625b71)
+//val Pink40 = Color(0xFF7D5260)
+
+val Green100 = Color(0xffffffff)
+val Green90 = Color(0xffccffcc)
+val Green80 = Color(0xff99ff99)
+val Green70 = Color(0xff66ff66)
+val Green60 = Color(0xff33ff33)      // Pd
+val Green50 = Color(0xff00ff00)
+val Green40 = Color(0xff00cc00)      // Pl
+val Green30 = Color(0xff009900)
+val Green20 = Color(0xff006600)
+val Green15 = Color(0xff005000)
+val Green10 = Color(0xff003300)
+val Green5 = Color(0xff001700)
+val Green0 = Color(0xff000000)
+
+
+val White100 = Color(0xffffffff)
+val White90 = Color(0xffe6e6e6)
+val White80 = Color(0xffcccccc)
+val White70 = Color(0xffb3b3b3)
+val White60 = Color(0xff999999)
+val White50 = Color(0xff808080)
+val White40 = Color(0xf0666666)
+val White30 = Color(0xff4d4d4d)
+val White20 = Color(0xff333333)
+val White10 = Color(0xff1a1a1a)
+val White0 = Color(0xff000000)
---
+++ trial_03/app/src/main/java/cz/matee/nemect/trial_03/ui/theme/Theme.kt
@@ -0,0 +1,138 @@
+package cz.matee.nemect.trial_03.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
+
+private val DarkColorScheme = darkColorScheme(
+    // 60 100 20 90 ??
+    primary = White100,
+//    onPrimary = Purple100,
+//    primaryContainer = Purple20,
+//    onPrimaryContainer = Purple90,
+    inversePrimary = White0,
+
+    //
+    secondary = White40,
+    onSecondary = White100,
+//    secondaryContainer = Blue20,
+//    onSecondaryContainer = Blue90,
+
+    //
+    tertiary = Green60,
+    onTertiary = Green0,
+//    tertiaryContainer = White20,
+//    onTertiaryContainer = White90,
+
+    //
+    background = White80,
+    onBackground = White100,
+
+    //
+    surface = White30,
+    onSurface = White100,
+//    surfaceVariant = Purple20,
+//    onSurfaceVariant = Purple100,
+    surfaceTint = White0,
+//    inverseSurface = Purple20,
+//    inverseOnSurface = Purple100,
+
+    //
+//    error = ,
+//    onError = ,
+//    errorContainer = ,
+//    onErrorContainer = ,
+
+    //
+    outline = White50,
+    outlineVariant = White20,
+
+    //
+//    scrim = ,
+)
+
+private val LightColorScheme = lightColorScheme(
+    // 40 100 90 10 ??
+    primary = White0,
+    onPrimary = White100,
+//    primaryContainer = Blue90,
+//    onPrimaryContainer = Blue10,
+//    inversePrimary = ,
+
+    secondary = White30,
+    onSecondary = White90,
+//    secondaryContainer = Purple90,
+//    onSecondaryContainer = Purple10,
+
+    tertiary = Green40,
+    onTertiary = Green100,
+//    tertiaryContainer = White90,
+//    onTertiaryContainer = White10,
+
+    //
+    background = White100,
+    onBackground = White0,
+
+    // 100 0 90 0
+    surface = Green80,
+    onSurface = Green0,
+//    surfaceVariant = Blue90,
+//    onSurfaceVariant = Blue0,
+    surfaceTint = White0,
+//    inverseSurface = Blue80,
+//    inverseOnSurface = Blue0,
+
+    //
+//    error = ,
+//    onError = ,
+//    errorContainer = ,
+//    onErrorContainer = ,
+
+    //
+    outline = White50,
+    outlineVariant = White80,
+
+    //
+//    scrim = ,
+)
+
+@Composable
+fun Trial_03Theme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
