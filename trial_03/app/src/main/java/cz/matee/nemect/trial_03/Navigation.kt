b83f6da8546c70a16fcac5c0f778366275a4d67b
Tomas Nemecek <nemect20@fit.cvut.cz>
2023-08-04 16:03:04 +0200

    trial_03: Extend navigation with other screens
    Extended:
    	four basic screens - home, search, library, premium
    	other - news, recent, settings
    Modified:
    	navigationItem - appearance of mainScreen logo at the bottom

--- trial_03/app/src/main/java/cz/matee/nemect/trial_03/Navigation.kt
+++ trial_03/app/src/main/java/cz/matee/nemect/trial_03/Navigation.kt
@@ -1,11 +1,14 @@
 package cz.matee.nemect.trial_03
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment.Companion.Bottom
-import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -14,6 +17,14 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import cz.matee.nemect.trial_03.presentation.home.HomeScreen
+import cz.matee.nemect.trial_03.presentation.home.HomeScreenVM
+import cz.matee.nemect.trial_03.presentation.library.LibraryScreen
+import cz.matee.nemect.trial_03.presentation.login.LoginScreen
+import cz.matee.nemect.trial_03.presentation.news.NewsScreen
+import cz.matee.nemect.trial_03.presentation.premium.PremiumScreen
+import cz.matee.nemect.trial_03.presentation.recent.RecentScreen
+import cz.matee.nemect.trial_03.presentation.search.SearchScreen
+import cz.matee.nemect.trial_03.presentation.settings.SettingsCreen
 import cz.matee.nemect.trial_03.ui.components.NavigationItem
 
 
@@ -25,19 +36,29 @@ sealed class Screen( val route: String ) {
     object SettingsScreen : Screen("settings")
     object NewsScreen : Screen("news")
     object RecentScreen : Screen("recent")
+    object LoginScreen : Screen("login")
 }
 
 data class MainScrens (
     val screen: Screen,
     val text: String,
     val iconID: Int,
-    var selected: Boolean = false
+    val selected: MutableState<Boolean> = mutableStateOf(false)
 )
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 fun Navigation() {
     val navController = rememberNavController()
+
+    val mainScreens = remember {
+        listOf(
+            MainScrens(Screen.HomeScreen, "Home", R.drawable.ic_home, mutableStateOf(true)),
+            MainScrens(Screen.SearchScreen, "Search", R.drawable.ic_search),
+            MainScrens(Screen.LibraryScreen, "Your library", R.drawable.ic_library),
+            MainScrens(Screen.PremiumScreen, "Premium", R.drawable.ic_premium)
+        )
+    }
 
     Scaffold( 
         bottomBar = {
@@ -47,26 +68,28 @@ fun Navigation() {
                     modifier = Modifier
                         .height(150.dp)
                         .background(
-                            Brush.verticalGradient(listOf(
-                                MaterialTheme.colorScheme.surface.copy(alpha = 0f),
-                                MaterialTheme.colorScheme.surface
-                            ))
+                            Brush.verticalGradient(
+                                listOf(
+                                    MaterialTheme.colorScheme.surface.copy(alpha = 0f),
+                                    MaterialTheme.colorScheme.surface
+                                )
+                            )
                         )
                     //Brush.verticalGradient(listOf(Color.Transparent, Color.Black))
                 ) {
-                    val mainScreens = listOf(
-                        MainScrens(Screen.HomeScreen, "Home", R.drawable.ic_home, true),
-                        MainScrens(Screen.SearchScreen, "Search", R.drawable.ic_search),
-                        MainScrens(Screen.LibraryScreen, "Your library", R.drawable.ic_library),
-                        MainScrens(Screen.PremiumScreen, "Premium", R.drawable.ic_premium)
-                    )
+
 
                     mainScreens.forEach { item ->
                         NavigationItem(
                             iconID = item.iconID,
                             text = item.text,
-                            selected = item.selected,
-                            onClick = { /*TODO*/ },
+                            selected = item.selected.value,
+                            onClick = {
+                                val selected = mainScreens.find { it.selected.value }
+                                selected?.selected?.value = false
+                                item.selected.value = true
+                                navController.navigate( item.screen.route )
+                            },
                             modifier = Modifier
                                 .weight(1f)
                                 .align(Bottom)
@@ -79,7 +102,28 @@ fun Navigation() {
             startDestination = Screen.HomeScreen.route
         ) {
             composable( route = Screen.HomeScreen.route ) {
-                HomeScreen()
+                HomeScreen( HomeScreenVM(), navController )
+            }
+            composable( route = Screen.SearchScreen.route ) {
+                SearchScreen( navController )
+            }
+            composable( route = Screen.LibraryScreen.route ) {
+                LibraryScreen()
+            }
+            composable( route = Screen.PremiumScreen.route ) {
+                PremiumScreen()
+            }
+            composable( route = Screen.SettingsScreen.route ) {
+                SettingsCreen( navController )
+            }
+            composable( route = Screen.LoginScreen.route ) {
+                LoginScreen()
+            }
+            composable( route = Screen.NewsScreen.route ) {
+                NewsScreen( navController )
+            }
+            composable( route = Screen.RecentScreen.route ) {
+                RecentScreen( navController )
             }
         }
     }
--- trial_03/app/src/main/java/cz/matee/nemect/trial_03/ui/components/NavigationItem.kt
+++ trial_03/app/src/main/java/cz/matee/nemect/trial_03/ui/components/NavigationItem.kt
@@ -1,13 +1,21 @@
 package cz.matee.nemect.trial_03.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import cz.matee.nemect.trial_03.ui.theme.Typography
 
 @Composable
@@ -24,6 +32,8 @@ fun NavigationItem(
     IconButton(
         onClick = onClick,
         modifier = modifier
+            .width(50.dp)
+            .height(70.dp)
     ) {
         Column {
             Icon(
@@ -33,7 +43,10 @@ fun NavigationItem(
             Text(
                 text = text,
                 style = Typography.labelSmall,
-                color = itemColor)
+                color = itemColor,
+                modifier = Modifier
+                    .align(CenterHorizontally)
+            )
         }
     }
 
