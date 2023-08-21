f7c25732fcc85067666db80a4db31fa80caf0d0a
Tomas Nemecek <nemect20@fit.cvut.cz>
2023-08-21 09:39:49 +0200

    trial_02: Add value resource file
    Value resource file contains common default variables as paddings,
    radiuses, etc.

---
+++ trial_02/app/src/main/java/cz/matee/nemect/trial_02/presentation/ui/theme/Value.kt
@@ -0,0 +1,9 @@
+package cz.matee.nemect.trial_02.presentation.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val AppBarRounded = 20.dp
+val ItemCardRounded = 10.dp
+
+val LargeIconSize = 80.dp
+val MediumIconSize = 40.dp
