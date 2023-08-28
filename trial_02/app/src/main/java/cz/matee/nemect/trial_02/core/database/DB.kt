6421750c83aaee2cf460439ee183cba2d1fd5f54
Tomas Nemecek <nemect20@fit.cvut.cz>
2023-08-28 11:04:08 +0200

    trial_02: Add local temporary database

--- trial_02/app/src/main/java/cz/matee/nemect/trial_02/core/database/DB.kt
+++ trial_02/app/src/main/java/cz/matee/nemect/trial_02/core/database/DB.kt
@@ -1,19 +1,49 @@
 package cz.matee.nemect.trial_02.core.database
 
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.text.toUpperCase
+import java.lang.Character.toUpperCase
 
 
 class DB {
+    //          VALUES
     private val languages = mutableStateListOf<Language>(
         Language("English","EN"),
         Language("Germany","NE"),
         Language("Czech","CZ")
     )
 
+    //          PROVIDERS
     fun provideLanguages(): MutableList<Language> {
         return languages
     }
 
+    fun provideLanguage(
+        languageCode: String
+    ): Language {
+        val lang = languages.find { it.sign == languageCode.uppercase() }
+        if ( lang == null )
+            throw NullPointerException("Failed to find language ${languageCode.uppercase()}.")
+        else
+            return lang
+    }
+
+    fun provideWords(
+        languageCode: String
+    ): MutableList<Word> {
+        val groups = provideLanguage(languageCode).groups
+        val words = mutableListOf<Word>()
+
+        groups.forEach {
+            words.addAll(it.words)
+        }
+
+        return words
+    }
+
+
+
+    //          MODIFIERS
     fun addLanguage(
         language: String,
         sign: String
