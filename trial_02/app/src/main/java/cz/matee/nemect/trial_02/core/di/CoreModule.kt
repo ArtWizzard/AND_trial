3cdca57462b72d9ab37660000611d3591d7a463b
Tomas Nemecek <nemect20@fit.cvut.cz>
2023-09-18 15:22:06 +0200

    trial_02: Update core module for di

--- trial_02/app/src/main/java/cz/matee/nemect/trial_02/core/di/CoreModule.kt
+++ trial_02/app/src/main/java/cz/matee/nemect/trial_02/core/di/CoreModule.kt
@@ -1,10 +1,14 @@
 package cz.matee.nemect.trial_02.core.di
 
+import cz.matee.nemect.trial_02.App
 import cz.matee.nemect.trial_02.core.database.DB
 import cz.matee.nemect.trial_02.navigation.NavigationManager
-import cz.matee.nemect.trial_02.presentation.screens.language.LanguageVM
 import cz.matee.nemect.trial_02.presentation.screens.home.HomeVM
-import cz.matee.nemect.trial_02.presentation.screens.home.dictionary.DictionaryHomeScreenVM
+import cz.matee.nemect.trial_02.presentation.screens.home.dictionary.DictionaryScreenVM
+import cz.matee.nemect.trial_02.presentation.screens.language.LanguageVM
+import cz.matee.nemect.trial_02.presentation.screens.settings.SettingsVM
+import cz.matee.nemect.trial_02.presentation.screens.home.test.TestVM
+
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
@@ -15,8 +19,15 @@ val coreModule get() = module {
     // Database
     single { DB() }
 
+    // App
+    single { App() }
+    single { App().baseContext }
+
     // View models
-    viewModelOf(::DictionaryHomeScreenVM)
+    viewModelOf(::SettingsVM)
+    viewModelOf(::DictionaryScreenVM)
     viewModelOf(::LanguageVM)
     viewModelOf(::HomeVM)
+    viewModelOf(::TestVM)
+
 }
